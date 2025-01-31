1b5f23ff49cbf2c1af68f3398cc7914e26bffdb0
Albani Rajata Malik <albeebro002@gmail.com>
2025-01-31 21:30:07 +0700

    Displaying loading state

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { Assistant } from "./assistants/openai";
+import { Assistant } from "./assistants/googleai";
+import { Loader } from "./components/Loader/Loader";
 import { Chat } from "./components/Chat/Chat";
 import { Controls } from "./components/Controls/Controls";
 import styles from "./App.module.css";
@@ -7,6 +8,7 @@ import styles from "./App.module.css";
 function App() {
   const assistant = new Assistant();
   const [messages, setMessages] = useState([]);
+  const [isLoading, setLoading] = useState(false);
 
   function addMessage(message) {
     setMessages((prevMessages) => [...prevMessages, message]);
@@ -14,19 +16,23 @@ function App() {
 
   async function handleContentSend(content) {
     addMessage({ content, role: "user" });
+    setLoading(true);
     try {
-      const result = await assistant.chat(content, messages);
+      const result = await assistant.chat(content);
       addMessage({ content: result, role: "assistant" });
     } catch (error) {
       addMessage({
         content: "Sorry, I couldn't process your request. Please try again!",
         role: "system",
       });
+    } finally {
+      setLoading(false);
     }
   }
 
   return (
     <div className={styles.App}>
+      {isLoading && <Loader />}
       <header className={styles.Header}>
         <img className={styles.Logo} src="/chat-bot.png" />
         <h2 className={styles.Title}>AI Chatbot</h2>
