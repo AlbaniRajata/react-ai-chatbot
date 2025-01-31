d8dc77cc1171d946a33451b403ec89db25332eff
Albani Rajata Malik <albeebro002@gmail.com>
2025-01-31 22:01:39 +0700

    Connect Google AI, Open AI for streaming and enabling real time chat streaming

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,18 @@ import styles from "./App.module.css";
 function App() {
   const assistant = new Assistant();
   const [messages, setMessages] = useState([]);
-  const [isLoading, setLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isStreaming, setIsStreaming] = useState(false);
+
+  function updateLastMessageContent(content) {
+    setMessages((prevMessages) =>
+      prevMessages.map((message, index) =>
+        index === prevMessages.length - 1
+          ? { ...message, content: `${message.content}${content}` }
+          : message
+      )
+    );
+  }
 
   function addMessage(message) {
     setMessages((prevMessages) => [...prevMessages, message]);
@@ -16,17 +27,30 @@ function App() {
 
   async function handleContentSend(content) {
     addMessage({ content, role: "user" });
-    setLoading(true);
+    setIsLoading(true);
     try {
-      const result = await assistant.chat(content);
-      addMessage({ content: result, role: "assistant" });
+      const result = await assistant.chatStream(content);
+      let isFirstChunk = false;
+
+      for await (const chunk of result) {
+        if (!isFirstChunk) {
+          isFirstChunk = true;
+          addMessage({ content: "", role: "assistant" });
+          setIsLoading(false);
+          setIsStreaming(true);
+        }
+
+        updateLastMessageContent(chunk);
+      }
+
+      setIsStreaming(false);
     } catch (error) {
       addMessage({
         content: "Sorry, I couldn't process your request. Please try again!",
         role: "system",
       });
-    } finally {
-      setLoading(false);
+      setIsLoading(false);
+      setIsStreaming(false);
     }
   }
 
@@ -40,7 +64,10 @@ function App() {
       <div className={styles.ChatContainer}>
         <Chat messages={messages} />
       </div>
-      <Controls isDisabled={isLoading} onSend={handleContentSend} />
+      <Controls
+        isDisabled={isLoading || isStreaming}
+        onSend={handleContentSend}
+      />
     </div>
   );
 }
